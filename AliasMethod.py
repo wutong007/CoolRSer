9f9265289d5b839f5454fa00d4c39b98a1bd732b
shenxiaolinZERO <2311513185@qq.com>
2018-05-23 15:44:37 +0800

    MLin Action-SVM-.

--- AliasMethod.py
+++ AliasMethod.py
@@ -8,5 +8,22 @@ __author__ = 'ScarlettZero'
 import numpy as np
 import pandas as pd
 
+def alias_setup(probs):
+    '''
+
+    :param probs: 某个概率分布
+    :return: Alias数组与Prob数组
+    '''
+    K =len(probs)
+    q =np.zeros(K) #对应Prob数组
+    J =np.zeros(K,dtype=np.int) #对应Alias数组
+
+    #Sort the data into the outcomes with probabilities
+    #that are larger and smaller than 1/K
+    smaller =[] #存储比1小的列
+    larger =[] #存储比1大的列
+
+    for kk,prob in enumerate(probs):
+        
 
 
